0f2013c1d332f4db24648cfb6d22253b74db07d8
Koen De Maesschalck <koen@silvermoongroup.com>
2016-02-12 22:13:31 +0100

    added code for handling authorisation - unfortunately it isn't working.

--- opa-integration/src/main/java/com/silvermoongroup/opaintegration/OpaAssessmentIntegration.java
+++ opa-integration/src/main/java/com/silvermoongroup/opaintegration/OpaAssessmentIntegration.java
@@ -4,10 +4,27 @@ import com.oracle.opa.AssessRequest;
 import com.oracle.opa.AttributeType;
 import com.oracle.opa.GlobalInstanceType;
 import com.oracle.opa.OutcomeStyleEnum;
+import org.apache.http.HttpException;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+import org.springframework.ws.transport.WebServiceMessageSender;
+import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 import org.springframework.xml.transform.StringResult;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 
@@ -34,11 +51,20 @@ import java.math.BigDecimal;
  */
 public class OpaAssessmentIntegration {
 
-
+    public static String contextRoot = "/lunos_rules/determinations-server/assess/soap/generic/12.2.1/computerloss_rules";
     public static String endpointURL = "http://192.168.10.25:7003/lunos_rules/determinations-server/assess/soap/generic/12.2.1/computerloss_rules";
 
     public static void main(String[] args){
-        System.out.println("com.silvermoongroup.opaintegration.OpaAssessmentIntegration");
+        System.out.println("args.length = "+args.length);
+        if (args.length == 2) {
+            populateEndpointURL(args[0], args[1]);
+            System.out.println("Using the following endpointUrl :\n"+endpointURL);
+        } else {
+            System.out.println("Provide the ip address and port as the first and the second argument respectively. \n" +
+                    "Example:\n" +
+                    OpaAssessmentIntegration.class.getSimpleName() + " 192.168.10.25 7003");
+            return;
+        }
 
         AssessRequest request = new AssessRequest();
         GlobalInstanceType globalInstance = new GlobalInstanceType();
@@ -69,14 +95,58 @@ public class OpaAssessmentIntegration {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setContextPath("com.oracle.opa");
 
-        WebServiceTemplate template = new WebServiceTemplate();
+        SaajSoapMessageFactory messageFactory = new SaajSoapMessageFactory();
+        messageFactory.afterPropertiesSet();
+        WebServiceTemplate template = new WebServiceTemplate(messageFactory);
         template.setMarshaller(marshaller);
         template.setUnmarshaller(marshaller);
+        template.setMessageSender(messageSender());
 
         template.setDefaultUri(endpointURL);
         StringResult result = (StringResult) template.marshalSendAndReceive(request);
         System.out.println(result);
 
 
+    }
+
+    private static class ContentLengthHeaderRemover implements HttpRequestInterceptor{
+
+        @Override
+        public void process(HttpRequest request, HttpContext context)
+                throws HttpException, IOException {
+
+            // fighting org.apache.http.protocol.RequestContent's
+            // ProtocolException("Content-Length header already present");
+            request.removeHeaders(HTTP.CONTENT_LEN);
+        }
+    }
+
+    public static HttpComponentsMessageSender messageSender() {
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setAuthenticationEnabled(true)
+                .build();
+
+        HttpClientBuilder httpClientBuilder = HttpClients.custom();
+
+        HttpClient httpClient = httpClientBuilder
+                .addInterceptorFirst(new ContentLengthHeaderRemover())
+                .setDefaultRequestConfig(requestConfig)
+                .setDefaultCredentialsProvider(credentialsProvider())
+                .build();
+
+        HttpComponentsMessageSender messageSender = new HttpComponentsMessageSender(httpClient);
+        return messageSender;
+    }
+
+    public static CredentialsProvider credentialsProvider() {
+        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("admin","password");
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, credentials);
+        return credentialsProvider;
+    }
+
+    private static void populateEndpointURL(String ipAddress, String port) {
+        endpointURL = "http://"+ipAddress+":"+port+contextRoot;
     }
 }
